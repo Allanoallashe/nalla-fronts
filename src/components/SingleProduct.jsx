4941adba9b1e8b294d4e6c947cf69c3c695b2dce
allan lenny <owinoallan77@gmail.com>
2023-08-11 21:51:00 +0300

    single products page

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,4 +1,5 @@
 import styles from '@/styles/Home.module.css'
+import { useState } from 'react'
  
   const heading = {
   backgroundImage: 'linear-gradient(to right bottom, #fa0588, #f7009a, #f000ae, #e400c4, #d300da, #c400e4, #b200ef, #9b01fa, #8e00fa, #7f00fa, #6e02fa, #5b05fa)',
@@ -7,11 +8,16 @@ import styles from '@/styles/Home.module.css'
   WebkitTextFillColor: 'transparent',
   MozTextFillColor:'transparent',
   backgroundSize: '10%',
-  marginTop:'70px',
-  textAlign: 'center',
+  textAlign: 'justify',
 }
 
-const SingleProduct = ({product}) => {
+const SingleProduct = ({ product }) => {
+  
+  const [activeImage, setActiveImage] = useState(product?.images[0])
+
+  const images = product.images
+ 
+
   return (
     <>
       <div
@@ -19,16 +25,42 @@ const SingleProduct = ({product}) => {
         style={{
           display: 'flex',
           alignItems: 'center',
-          justifyContent: 'space-evenly',
-          gap: 30,
+          justifyContent: 'center',
+          flexWrap:'wrap',
+          columnGap: 30,
           padding:'10px 36px'
         }}
       >
         <div className={styles.singleProduct}>
-        <img height={180} src={product.images[0]} alt='singles' />
+          <div className={styles.imageCont}>
+            <img height={300} src={activeImage} alt='singles' />
+          </div>
+
+          <div
+            style={{
+              display: 'flex',
+              gap:8,
+            }}
+          >
+              {
+                images.map(image=>(
+                <img
+                  src={image}
+                  height={50}
+                  onClick={() => {
+                  setActiveImage(image)
+                  }}
+                  alt='all' />
+                  ))
+                }
+            </div>
+         </div>
+          <div className={styles.descriptions}>
+            <h2 style={heading}>{ product.title }</h2>
+            <h4 >{product.description}</h4>
+          </div>
         </div>
-        <h4 style={heading}>{product.description}</h4>
-      </div>
+   
     </>
    )
 }
